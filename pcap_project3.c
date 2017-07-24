2cb11e27cd1d8db85277dd8b44b45e1501150c30
gokwangwoo <rhrhkddn22@naver.com>
2017-07-24 18:24:03 +0900

    send source code

--- pcap_project3.c
+++ pcap_project3.c
@@ -1,30 +1,44 @@
 #define ETHER_ADDR_LEN	6
 #define SIZE_ETHERNET	14
+#define INET_ADDRSTRLEN	16
 #include <pcap.h>
 #include <net/ethernet.h>
-#include<netinet/ip.h>
-#include<arpa/inet.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <stdint.h>
 #include <string.h> 
 #include <stdio.h>
 struct sniff_ethernet {
-	u_char h_dest[ETHER_ADDR_LEN]; /* Destination host address */
-	u_char h_source[ETHER_ADDR_LEN]; /* Source host address */
-	u_short h_proto; /* IP? ARP? RARP? etc */
+	//u_char h_dest[ETHER_ADDR_LEN]; /* Destination host address */
+	//u_char h_source[ETHER_ADDR_LEN]; /* Source host address */
+	//u_short h_proto; /* IP? ARP? RARP? etc */
+
+	uint8_t h_dest[ETHER_ADDR_LEN]; /* Destination host address */
+	uint8_t h_source[ETHER_ADDR_LEN]; /* Source host address */
+	uint16_t h_proto; /* IP? ARP? RARP? etc */
 };
 
 struct sniff_ip {
-	u_char ip_vhl;		/* version << 4 | header length >> 2 */
-	u_char ip_tos;		/* type of service */
-	u_short ip_len;		/* total length */
-	u_short ip_id;		/* identification */
-	u_short ip_off;		/* fragment offset field */
+	//u_char ip_vhl;		/* version << 4 | header length >> 2 */
+	//u_char ip_tos;		/* type of service */
+	//u_short ip_len;		/* total length */
+	//u_short ip_id;		/* identification */
+	//u_short ip_off;		/* fragment offset field */
+	uint8_t ip_vhl;		/* version << 4 | header length >> 2 */
+	uint8_t ip_tos;		/* type of service */
+	uint16_t ip_len;		/* total length */
+	uint16_t ip_id;		/* identification */
+	uint16_t ip_off;		/* fragment offset field */
 	#define IP_RF 0x8000		/* reserved fragment flag */
 	#define IP_DF 0x4000		/* dont fragment flag */
 	#define IP_MF 0x2000		/* more fragments flag */
 	#define IP_OFFMASK 0x1fff	/* mask for fragmenting bits */
-	u_char ip_ttl;		/* time to live */
-	u_char ip_p;		/* protocol */
-	u_short ip_sum;		/* checksum */
+	//u_char ip_ttl;		/* time to live */
+	//u_char ip_p;		/* protocol */
+	//u_short ip_sum;		/* checksum */
+	uint8_t ip_ttl;		/* time to live */
+	uint8_t ip_p;		/* protocol */
+	uint16_t ip_sum;		/* checksum */
 	struct in_addr ip_src,ip_dst; /* source and dest address */
 };
 #define IP_HL(ip)	(((ip)->ip_vhl) & 0x0f)
@@ -35,13 +49,17 @@ struct sockaddr_in source,dest;
 typedef u_int tcp_seq;
 
 struct sniff_tcp {
-        u_short th_sport;               /* source port */
-        u_short th_dport;               /* destination port */
+        //u_short th_sport;               /* source port */
+	   uint16_t th_sport;
+        //u_short th_dport;               /* destination port */
+	   uint16_t th_dport;
         tcp_seq th_seq;                 /* sequence number */
         tcp_seq th_ack;                 /* acknowledgement number */
-        u_char  th_offx2;               /* data offset, rsvd */
-#define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
-        u_char  th_flags;
+        //u_char  th_offx2;               /* data offset, rsvd */
+	   uint8_t  th_offx2;
+	   #define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
+        //u_char  th_flags;
+	   uint8_t th_flags;
         #define TH_FIN  0x01
         #define TH_SYN  0x02
         #define TH_RST  0x04
@@ -51,9 +69,13 @@ struct sniff_tcp {
         #define TH_ECE  0x40
         #define TH_CWR  0x80
         #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
-        u_short th_win;                 /* window */
-        u_short th_sum;                 /* checksum */
-        u_short th_urp;                 /* urgent pointer */
+        //u_short th_win;                 /* window */
+        //u_short th_sum;                 /* checksum */
+        //u_short th_urp;                 /* urgent pointer */
+
+	   uint16_t th_win;                 /* window */
+        uint16_t th_sum;                 /* checksum */
+        uint16_t th_urp;                 /* urgent pointer */
 };
 
 void print_packet_data(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
@@ -130,7 +152,10 @@ void print_packet_data(u_char *args, const struct pcap_pkthdr *header, const u_c
 	const struct sniff_ip *ip;              /* The IP header */
 	const struct sniff_tcp *tcp;            /* The TCP header */
 	const char *payload;                    /* Packet payload */
-
+	///inet_ntop
+	char sourceIp[INET_ADDRSTRLEN];
+	char destIp[INET_ADDRSTRLEN];
+	///
 	int size_ip;
 	int size_tcp;
 	int size_payload;
@@ -150,8 +175,12 @@ void print_packet_data(u_char *args, const struct pcap_pkthdr *header, const u_c
 	}
 	printf("IP_HL: %d, IP_V: %d\n", IP_HL(ip), IP_V(ip));
 	/* print source and destination IP addresses */
-	printf("       Source IP: %s\n", inet_ntoa(ip->ip_src));
-	printf("       Destionation IP: %s\n", inet_ntoa(ip->ip_dst));
+	//printf("			Source IP: %s\n", inet_ntop(AF_INET, &(ip->ip_src), sourceIp, 16));
+	printf("			Source IP: %s\n", inet_ntop(AF_INET, &(ip->ip_src), sourceIp, INET_ADDRSTRLEN));
+	//printf("       Source IP: %s\n", inet_ntoa(ip->ip_src));
+	//printf("			Destination IP: %s\n", inet_ntop(AF_INET, &(ip->ip_dst), destIp, 16));
+	printf("			Destination IP: %s\n", inet_ntop(AF_INET, &(ip->ip_dst), destIp, INET_ADDRSTRLEN));
+	//printf("       Destionation IP: %s\n", inet_ntoa(ip->ip_dst));
 	
 	/* determine protocol */	
 	switch(ip->ip_p) {
